92d500b576e2f7c44c34d1fb1aa24d994f45c167
Alvaro Valencia <alvarovalenciavp@gmail.com>
2021-06-10 10:05:46 +0200

    feat: created templates for Spark DataFrameMonitor

---
+++ src/main/scala/es/us/idea/dmn4spark/diagnosis/spark/DataFrameDQMonitor.scala
@@ -0,0 +1,29 @@
+package es.us.idea.dmn4spark.diagnosis.spark
+
+import es.us.idea.dmn4spark.diagnosis.graph.DMN4DQTree
+import org.apache.spark.sql.DataFrame
+
+/**
+ * Features (todo)
+ * - get underlying df
+ * - get usability profiles and counts (json representation + )
+ * - generate basic cost model
+ * - get usability profiles which meet certain conditions: BR.DUD (only get those existing in the DF??)
+ * - insert cost model (json str)
+ * - apply inserted cost model => generate new object: "DataFrameDQReparation"
+ *
+ * - DFDQMonitors can be created from: df + dqmn4dqtree
+ *    - any dataframe including DQ results, trees, etc
+ *    - DFDQMonitors, if they are saved, a row including only the full tree must be included.
+ *
+ *
+ */
+class DataFrameDQMonitor(df: DataFrame, dmn4dqTree: DMN4DQTree) {
+
+  def df(): DataFrame = df
+
+
+
+
+
+}
